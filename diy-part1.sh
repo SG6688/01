ba4d2e004c30981279f5ba1093f0699a5975589c
SG6688 <77781450+SG6688@users.noreply.github.com>
2021-02-08 09:38:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+#git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 #git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 #git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 #git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
