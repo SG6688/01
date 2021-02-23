5af0d9214637bb01fa1d49986d82245a85fa8946
SG6688 <77781450+SG6688@users.noreply.github.com>
2021-02-23 09:01:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-
+Replace_File zzz-default-settings package/lean/default-settings/files zzz-default-settings
 # 修改默认
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 
