567c2df571cf9fc5c3b08a9f615ebb2b4eec7012
SG6688 <77781450+SG6688@users.noreply.github.com>
2021-03-05 17:17:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,8 +25,8 @@ sed -i '$a src-git danshui https://github.com/281677160/openwrt-package' feeds.c
 #src-git garypang13 https://github.com/SG6688/openwrt-packages.git
 #src-git danshui https://github.com/281677160/openwrt-package.git
 
-#git clone https://github.com/kenzok8/small.git
-#git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone https://github.com/kenzok8/small.git
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 #git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 #git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 #git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
